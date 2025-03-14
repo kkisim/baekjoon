47b94a176c3a4287f56445032cf36a309271df3d
kkisim <163362240+kkisim@users.noreply.github.com>
2025-03-14 14:43:29 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2404 ms, Memory: 324328 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,28 +1,38 @@
-import java.io.*;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        // 입력 최적화
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-
-        // 정렬 (O(N log N))
-        Arrays.sort(arr);
-
-        // 출력 최적화
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            sb.append(arr[i]).append('\n');
-        }
-
-        // System.out 한 번만
-        System.out.print(sb);
+    public static void main(String[] args) {
+    	Sort s =new Sort();
+    	s.setting();
+    	s.sorting();
     }
 }
+
+class Sort{
+	static Scanner sc = new Scanner(System.in);
+	int num;
+	int arr[];
+	
+	void setting() {
+		num = sc.nextInt();
+		arr = new int[num];
+		for(int i = 0; i < num; i++) {
+			arr[i] = sc.nextInt();
+		}
+	}
+	void sorting() {
+		Arrays.sort(arr);
+		print();
+	}
+	void print() {
+	    StringBuilder sb = new StringBuilder();
+	    for (int i = 0; i < num; i++) {
+	        sb.append(arr[i]).append('\n');
+	    }
+	    System.out.print(sb);
+	}
+
+	}
+
+
