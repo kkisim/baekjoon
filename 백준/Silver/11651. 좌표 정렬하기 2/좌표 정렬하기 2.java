efdf129b4d5699e5195f9defcfc71f39f8f5fd75
kkisim <163362240+kkisim@users.noreply.github.com>
2025-03-16 19:12:40 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1800 ms, Memory: 83500 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,56 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	 Sort s = new Sort();
+         s.read();
+         s.sorting();
+         s.print();
+       
+        
+    }
+}
+
+class number {
+    int num1;
+    int num2;
+
+    number(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+}
+
+class Sort {
+    int num;
+    List<number> numbers;
+
+    void read() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        num = Integer.parseInt(br.readLine());
+        numbers = new ArrayList<>();
+
+        for (int i = 0; i < num; i++) {
+            String[] input = br.readLine().split(" ");
+            int num1 = Integer.parseInt(input[0]);
+            int num2 = Integer.parseInt(input[1]);
+            numbers.add(new number(num1, num2));
+
+        }
+    }
+    void sorting() {
+        numbers.sort((a, b) -> {
+        	 if (a.num2 != b.num2) return a.num2 - b.num2; 
+             return a.num1 - b.num1;                               
+        });
+    }
+
+    void print() {
+        for (number n : numbers) {
+            System.out.println(n.num1 + " " + n.num2);
+        }
+    }
+ 
+}
