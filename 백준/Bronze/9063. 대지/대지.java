882f0e70b4a16c4cb57637af787bb7eb0fca348a
kkisim <163362240+kkisim@users.noreply.github.com>
2025-03-30 19:22:36 +0900

    [Bronze III] Title: 대지, Time: 248 ms, Memory: 34332 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,41 @@
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        C w = new C();
+        w.setting();
+        w.print();
+    }
+}
+
+class C {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int N;
+    int minX = Integer.MAX_VALUE;
+    int maxX = Integer.MIN_VALUE;
+    int minY = Integer.MAX_VALUE;
+    int maxY = Integer.MIN_VALUE;
+
+    void setting() throws IOException {
+        N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            int x = Integer.parseInt(input[0]);
+            int y = Integer.parseInt(input[1]);
+            if (x < minX) minX = x;
+            if (x > maxX) maxX = x;
+            if (y < minY) minY = y;
+            if (y > maxY) maxY = y;
+        }
+        int width = maxX - minX;
+        int height = maxY - minY;
+        int area = width * height;
+        sb.append(area).append('\n');
+    }
+
+    void print() {
+        System.out.print(sb);
+    }
+}
