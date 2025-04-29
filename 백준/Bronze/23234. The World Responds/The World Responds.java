521c5b1c634ebfb5199124ee71c94ea61c66241b
kkisim <163362240+kkisim@users.noreply.github.com>
2025-04-29 13:12:59 +0900

    [Bronze V] Title: The World Responds, Time: 60 ms, Memory: 11336 KB -BaekjoonHub

---
+++ 백준/Bronze/23234. The World Responds/The World Responds.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        C w = new C();
+        w.setting();
+        w.print();
+    }
+}
+
+class C {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    void setting() throws IOException {
+      sb.append("The world says hello!");
+    }
+
+    void print() {
+        System.out.print(sb);
+    }
+}
