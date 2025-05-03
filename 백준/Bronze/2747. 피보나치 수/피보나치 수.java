91c4f3ed3ae26a34d4c23db4e1e9809490ef16ad
kkisim <163362240+kkisim@users.noreply.github.com>
2025-05-03 19:47:08 +0900

    [Bronze II] Title: 피보나치 수, Time: 68 ms, Memory: 11548 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        C w = new C();
+        w.setting();
+        w.print();
+    }
+}
+
+class C {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    void setting() throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 2]; 
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        sb.append(dp[n]).append('\n');
+    }
+
+    void print() {
+        System.out.print(sb);
+    }
+}
