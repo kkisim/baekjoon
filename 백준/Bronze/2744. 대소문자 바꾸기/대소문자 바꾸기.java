2c4771529f125e44d722dae3696f8c8a8958209d
kkisim <163362240+kkisim@users.noreply.github.com>
2025-05-20 09:03:47 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 68 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        C w = new C();
+        w.setting();
+        w.print();
+    }
+}
+
+class C {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    void setting() throws IOException {
+        String input = br.readLine();
+
+        for (char c : input.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                sb.append(Character.toLowerCase(c));
+            } else {
+                sb.append(Character.toUpperCase(c));
+            }
+        }
+    }
+
+    void print() {
+        System.out.print(sb);
+    }
+}
