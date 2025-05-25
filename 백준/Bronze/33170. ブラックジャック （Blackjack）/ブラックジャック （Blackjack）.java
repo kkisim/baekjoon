8b4182dace6dd2043b2fb17f3ea713285565e002
kkisim <kisimoko@gmail.com>
2025-05-25 16:38:49 +0900

    [Bronze V] Title: ブラックジャック (Blackjack), Time: 64 ms, Memory: 11468 KB -BaekjoonHub

---
+++ 백준/Bronze/33170. ブラックジャック （Blackjack）/ブラックジャック （Blackjack）.java
@@ -0,0 +1,31 @@
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        C w = new C();
+        w.setting();
+        w.print();
+    }
+}
+
+class C {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    void setting() throws IOException {
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        if (a + b + c <= 21) {
+            sb.append(1);
+        } else {
+            sb.append(0);
+        }
+    }
+
+    void print() {
+        System.out.println(sb);
+    }
+}
