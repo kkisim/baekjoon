61ad0f18e70530bdfc814a4ec815391c8010f805
kkisim <kisimoko@gmail.com>
2025-11-24 19:45:45 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 820 ms, Memory: 92280 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,78 @@
+	import java.io.*;
+	import java.math.BigInteger;
+	import java.util.*;
+	
+	public class Main {
+	    public static void main(String[] args) throws Exception {
+	        C w = new C();
+	        w.setting();
+	        w.print();
+	    }
+	}
+	
+	class C {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StringBuilder sb = new StringBuilder();
+	    StringTokenizer st;
+	    ArrayList<Integer>[] graph;
+	    boolean visited [] ;
+	    int count[] ;
+	    int c = 1;
+	    void setting() throws Exception {
+
+	    	st = new StringTokenizer(br.readLine());
+	        
+	        int V  = Integer.parseInt(st.nextToken()); //정점
+	        int E  = Integer.parseInt(st.nextToken()); //간선
+	        int R  = Integer.parseInt(st.nextToken()); //시작노드
+	        	
+	        graph = new ArrayList[V + 1]; 
+	        for (int i = 1; i <= V; i++) {
+	            graph[i] = new ArrayList<>();
+	        }
+	        
+	        for (int i = 0; i < E; i++) {
+	        	st = new StringTokenizer(br.readLine());
+	            int a = Integer.parseInt(st.nextToken());
+	            int b = Integer.parseInt(st.nextToken());
+	            // 무방향 그래프이기 때문에 양쪽에서 더해준다
+	            graph[a].add(b);
+	            graph[b].add(a); 
+	        }
+	        
+	        for (int i = 1; i <= V; i++) {
+	            Collections.sort(graph[i],Collections.reverseOrder()); //내림차순 정렬
+	        }
+	        
+	        visited = new boolean[V + 1];  //방문 여부 체크하기
+	        count = new int[V + 1]; 	//방문 순서 체크 하기
+	        bfs(R); 
+	  
+	    }
+	    void bfs(int R) {
+	    	 Queue<Integer> queue = new LinkedList<>();
+	         visited[R] = true; //방문했다는 표시
+	         count[R] = c++;	//방문 순서 설정
+	         queue.add(R);		//탐색시작
+	         while(!queue.isEmpty()) {
+	        	 int current = queue.poll();
+	        	 for(int n : graph[current]) {
+	        		 if(!visited[n]) {
+	        			 visited[n] = true;
+	        			 count[n]=c++;
+	        			 queue.add(n);
+	        		 }
+	        	 }
+	         }
+	    	
+	    }
+
+	    void print() {
+	    	for (int i = 1; i < count.length; i++) {
+	            sb.append(count[i]).append("\n");
+	        }
+	    	System.out.print(sb.toString());
+	        
+	    }
+	    
+	}
